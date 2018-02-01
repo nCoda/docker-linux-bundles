cca761b78e5383dc2c367aa575e6caa40c5163de
Christopher Antila <christopher@antila.ca>
2018-02-01 14:51:17 -0500

    Update the DNF packages installed

--- scripts/dnf.sh
+++ scripts/dnf.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-dnf update -y
-dnf install -y python2 python2-virtualenv python2-devel
-dnf install -y git
+# dnf update -y
+dnf install -y \
+    python2 python2-virtualenv python2-devel \
+    git \
+    gcc \
+    libxml2-devel libxslt-devel \
+    xz
 dnf clean all
